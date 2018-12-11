eeedc2a54926b53c670cf32faa30611b2afa6346
McKay Palmer <mckaypalmer@gmail.com>
2018-12-11 10:18:01 -0700

    Adding alert to type password

--- Project 4 Founders Directory Foundation/Founders Directory/LoginViewController.swift
+++ Project 4 Founders Directory Foundation/Founders Directory/LoginViewController.swift
@@ -12,8 +12,8 @@ import LocalAuthentication
 class LoginViewController : UITableViewController {
     
     // MARK: - Constants
-    let myContext = LAContext()
-    let myLocalizedReasonString = "Authenticate to view founders"
+    let context = LAContext()
+    let localizedReasonString = "Authenticate to view founders"
 
     // MARK: - Properties
 
@@ -33,11 +33,13 @@ class LoginViewController : UITableViewController {
         requestLogin()
     }
     
-//    override func viewDidLoad() {
-//        super.viewDidLoad()
-//
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
 //        ApiHelper.logout()
-//    }
+//        requestLogin()  //call to request login immediately
+//        loginAlert()
+    }
 
     // MARK: - Private helpers
 
@@ -52,23 +54,45 @@ class LoginViewController : UITableViewController {
         self.screenView = screenView
     }
     
-//    private func loginAlert() {
-//        let la = UIAlertController(title: "Please Sign In",
-//                                      message: "Enter Username and Password",
-//                                      preferredStyle: UIAlertController.Style.alert)
+    private func loginAlert() {
+        print("loginAlert()")
+        let la = UIAlertController(
+                                    title: "Please Sign In",
+                                    message: "Enter Username and Password",
+                                    preferredStyle: .alert)
+        let loginAction = UIAlertAction(title: "Log In", style: .default) {
+            (action) in
+            let userField = la.textFields![0] // Force unwrapping because we know it exists.
+            print(userField.text ?? "")
+            self.password = userField.text ?? ""
+            self.textLogin("user", self.password)
+        }
+        
+        let cancelAction = UIAlertAction(title: "Cancel", style: .cancel, handler: nil)
+        
+        la.addAction(loginAction)
+        la.addAction(cancelAction)
+        
+        la.addTextField {
+            (textField) in
+            textField.placeholder = "Enter Password"
+            textField.isSecureTextEntry = true
+        }
+        
+        self.present(la, animated: true)
 //        la.addTextField { (textField) in
 //            textField.text = "Username"
 //        }
 //        la.addTextField { (textField) in
 //            textField.text = "Password"
 //        }
-//
+
 //        la.addAction(UIAlertAction(title: "OK", style: .default, handler: { [weak la] (_) in
 //            let userField = la!.textFields![0] // Force unwrapping because we know it exists.
 //            let passwordField = la!.textFields![1]
 //            print("User field: \(userField.text ?? "user") Password: \(passwordField.text ?? "secret")")
 //        }))
-//    }
+    }
     
     private func showAlert(_ alertTitle: String, _ alertMessage: String) {
         let alert = UIAlertController(title: "\(alertTitle)",
@@ -82,37 +106,34 @@ class LoginViewController : UITableViewController {
         networkIndicator.startAnimating()
         displayScreenView()
         
-        if myContext.canEvaluatePolicy(.deviceOwnerAuthenticationWithBiometrics, error: &biometricError) {
-            
-            myContext.evaluatePolicy(.deviceOwnerAuthenticationWithBiometrics, localizedReason:
-                                                                                    myLocalizedReasonString) {
+        if context.canEvaluatePolicy(.deviceOwnerAuthenticationWithBiometrics, error: &biometricError) {
+
+            context.evaluatePolicy(.deviceOwnerAuthenticationWithBiometrics, localizedReason:
+                                                                                    localizedReasonString) {
                 [unowned self] (success, authenticationError) in
                 
                 DispatchQueue.main.async {
                     if success {
                         self.textLogin(self.username, self.password)
-//                        if let foundersApp = UIApplication.shared.delegate as? AppDelegate {
-//                            print("successful biometric Login")
-//                            foundersApp.displayMasterScene(animated: true)
-//                        } else {
-//                            self.showAlert("Unexpected Login Error Occured", "")
-//                        }
                     } else { // catch biometric errors
 //                        self.loginAlert()
                         print("username and password")
 //                        self.textLogin()
 //                        self.loginAlert()
-                        self.showAlert("Authentication failed", "You could not be verified; please try again.")
+//                        self.showAlert("Authentication failed", "You could not be verified; please try again.")
+                        // add action to display biometric login again
+                        self.loginAlert()
                     }
                 }
             }
         } else { // no biometry
 //            textLogin()
-            showAlert("Login", "Enter Username and Password")
+//            showAlert("Login", "Enter Username and Password")
+            print("need to allow biometry")
         }
     }
     
-private func textLogin(_ username: String, _ password: String) {
+    private func textLogin(_ username: String, _ password: String) {
         ApiHelper.shared.login(username, password) { [weak self] (message) in
             DispatchQueue.main.async {
                 if let failureMessage = message {
